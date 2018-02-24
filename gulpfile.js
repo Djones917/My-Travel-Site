77fe116641f1fa407ac736760c04b52351ddc082
Derek <nightwing917@hotmail.com>
2018-02-23 20:29:25 -0700

    first css arch

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,13 @@
 // Note: you don't have to use 'var' on the 'watch' variable. Just type it out and separate both variables with a comma!
+// Remember you have to add these packages with npm and gulp!
 var gulp = require('gulp'),
-watch = require('gulp-watch'); // example var watch.
+watch = require('gulp-watch'), // example var watch.
+postcss = require('gulp-postcss'),
+autoprefixer = require('autoprefixer'),
+cssvars = require('postcss-simple-vars'),
+nested = require('postcss-nested'),
+cssImport = require('postcss-import');
+
 
 //notice gulp default!
 gulp.task('default', function(){
@@ -12,8 +19,12 @@ gulp.task('html', function(){
 	console.log("Imagine something useful being done to your html here!");
 });
 
+//cssImport should be at the beginning!
 gulp.task('styles', function(){
-	console.log("Imagine Sass or PostCSS running here!");
+	//console.log("Imagine Sass or PostCSS running here!");
+	return gulp.src('./app/assets/styles/styles.css')
+	.pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
+	.pipe(gulp.dest('./app/temp/styles'));
 });
 
 gulp.task('watch', function(){
